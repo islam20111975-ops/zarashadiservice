65c23837ac2084efe5ac98da346dc0da3f589330
islam20111975-ops <islam20111975@gmail.com>
2026-09-20 16:10:52 +0100

    Build Zara Shadi Service website

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"Zara Shadi Service",description:"Aapka Rishta, Hamari Zimmedari"};
+export default function RootLayout({children}){return <html lang="hi"><body>{children}</body></html>}
