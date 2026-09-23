b18bc0d0199b37879b519034964e88dc47f9e9b6
islam20111975-ops <islam20111975@gmail.com>
2026-09-23 17:52:19 +0100

    Sort recharge history newest first

--- app/account/page.js
+++ app/account/page.js
@@ -21,7 +21,7 @@ export default function Account(){
     );
     const unsubReq=onSnapshot(
       query(collection(db,"walletRechargeRequests"),where("uid","==",user.uid)),
-      s=>setRequests(s.docs.map(d=>({id:d.id,...d.data()})))
+      s=>setRequests(s.docs.map(d=>({id:d.id,...d.data()})).sort((a,b)=>(b.createdAt?.seconds||0)-(a.createdAt?.seconds||0)))
     );
     const unsubUser=onSnapshot(
       doc(db,"users",user.uid),
